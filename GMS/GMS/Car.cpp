d47c9a56f2c793cd2e1779c6de2128a314083b14
Abdelrahman222-prog <es.abdelrahman.adel2025@alexu.edu.eg>
2026-08-13 18:31:25 +0200

    Add .vs folder to .gitignore

---
+++ GMS/GMS/Car.cpp
@@ -0,0 +1,17 @@
+#include "Car.h"
+
+Car::Car(int num, string name, int age, string type, string team, float spd, float cap) : carNumber(num), fullName(name), Age(age), Type(type), racing_Team(team), Speed(spd), Capacity(cap)
+{
+	calc_per_score(Speed, Capacity);
+	cout << "Car successfully checked_in" << endl;
+}
+
+void Car::diplay_info()
+{
+	cout << "Car data: \n";
+	cout << "Car number : " << carNumber << "\tCar name : " << fullName << "\tAge in racing seasons: " << Age << endl;
+	cout << "Car type : " << Type << "\tRacing team : " << racing_Team << "\tSpeed: " << Speed << endl;
+	cout << "\tCapacity: " << Capacity << endl;
+}
+
+
