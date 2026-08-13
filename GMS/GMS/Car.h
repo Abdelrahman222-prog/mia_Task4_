fd60824ddf9206fdaef527c0157af426a83ee19f
Abdelrahman222-prog <es.abdelrahman.adel2025@alexu.edu.eg>
2026-08-13 18:37:08 +0200

    Added base Car class

---
+++ GMS/GMS/Car.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+class Car//abstract class
+{
+private :
+	int carNumber;
+	string fullName;
+	int Age;
+	string Type;
+	string racing_Team;
+	float Speed;
+	float Capacity;
+	float perf_score;
+public:
+	Car(int num, string name, int age, string type, string team, float spd, float cap);
+	virtual void calc_per_score(float s, float cap)=0;
+	virtual void diplay_info();
+};
+
